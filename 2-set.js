6c7331fbb6c60ac329611fbd544284daf621b4ac
kamal <kamade123@gmail.com>
2022-09-09 16:49:52 +0100

    tested qall set propeertes

--- 2-set.js
+++ 2-set.js
@@ -82,3 +82,27 @@ function mySet() {
         })
     } 
 }
+
+
+var setA = new mySet();
+var setB = new mySet();
+setA.add("a");
+setB.add("b");
+setB.add("c");
+setB.add("a");
+setB.add("d");
+console.log(setA.subset(setB))
+console.log(setA.intersection(setB).values())
+console.log(setB.difference(setA).values())
+
+var setC = new Set();
+var setD = new Set();
+setC.add("a");
+setD.add("b");
+setD.add("c");
+setD.add("a");
+setD.add("d");
+console.log(setD.values())
+setD.delete("a")
+console.log(setD.has("a"));
+console.log(setD.add("d"));
