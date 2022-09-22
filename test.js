221bd72474bc2edd43705c7d0c539c7b272827b4
kamal <kamade123@gmail.com>
2022-09-22 21:13:55 +0100

    added permute array solution

--- test.js
+++ test.js
@@ -136,23 +136,150 @@
 // let a = 9;
 // flippingBits(a);
 
-function diagonalDifference(arr) {
-    // Write your code here
-    let n =arr.length;
-    let d1 = 0;
-    let d2 = 0;
+// function diagonalDifference(arr) {
+//     // Write your code here
+//     let n =arr.length;
+//     let d1 = 0;
+//     let d2 = 0;
     
-    for(let i=0; i < n; i++ ){
-        for(let j=0; j < n; j++){
-            if(i === j){
-                d1 += arr[i][j];
-            }
-            if (i + j === n-1){
-                d2 += arr[i][j];
-            }
+//     for(let i=0; i < n; i++ ){
+//         for(let j=0; j < n; j++){
+//             if(i === j){
+//                 d1 += arr[i][j];
+//             }
+//             if (i + j === n-1){
+//                 d2 += arr[i][j];
+//             }
+//         }
+//     }
+//     return Math.abs(d1 - d2);
+// }
+// let input = [[1, 2, 3], [24, 5, 6],  [9, 8, 9]];
+// diagonalDifference(input);
+
+
+// function countingSort(arr) {
+//     // Write your code here
+//     let range = [];
+    
+//     for(let i = 0; i < 100; i++){
+//         range.push(i)
+//     }
+//     let res = []
+
+//     for(const num of range) { 
+//         let temp = 0;
+//         for(const val of arr){
+        
+//             if(val === num) {
+//                 temp++;
+//             }
+//         }
+//         res.push(temp);
+//     }
+//     return res;
+// }
+
+// let input = [1, 1, 2, 3, 1]
+// countingSort(input);
+
+// function pangrams(s) {
+//     // Write your code here
+//     const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+//     const alphabet = alpha.map((x) => String.fromCharCode(x));
+
+//     const textUpper = s.toUpperCase()
+//     const check = []
+//     alphabet.forEach(a =>{
+//         let includes = textUpper.includes(a);
+//         check.push(includes)
+//         // console.log(includes);
+//     })
+
+//     // const res = check.includes(false);
+//     if(check.includes(false)) {
+//         return 'not pangram'
+//     } else {
+//         return 'pangram'
+//     }
+
+// }
+
+
+function twoArrays(k, A, B) {
+    // Write your code here
+    let check = []
+    let newA = A.sort((a, b)=> {
+        if (a === Infinity) {
+            return 1;
+        } else if(isNaN(a)){
+            return -1;
+        } else {
+            return a - b;
         }
+    });
+
+    let newB = B.sort((a, b)=> {
+        if (a === Infinity) {
+            return 1;
+        } else if(isNaN(a)){
+            return -1;
+        } else {
+            return b - a;
+        }
+    });
+
+    // console.log(newA)
+    // console.log(newB)
+    for(let i = 0; i < A.length; i++){
+        if(A[i] +  B[i] >= k ){
+            check.push('YES')
+            // console.log('YES')
+        } else {
+            check.push('NO')
+            // console.log('NO')
+        }
+        
     }
-    return Math.abs(d1 - d2);
+    if(check.includes('NO')){
+        console.log('final: NO')
+    } else {
+        console.log('YES')
+    }
+    
 }
-let input = [[1, 2, 3], [24, 5, 6],  [9, 8, 9]];
-diagonalDifference(input);
+
+// 18 73 55 59 37 13 1 33
+// 81 11 77 49 65 26 29 49
+// 91
+
+// 18 94
+// 84 2 50 51 19 58 12 90 81 68 54 73 81 31 79 85 39 2
+// 53 102 40 17 33 92 18 79 66 23 84 25 38 43 27 55 8 19
+// 8 88
+// 66 66 32 75 77 34 23 35
+// 61 17 52 20 48 11 50 5
+// 14 45
+// 11 16 43 5 25 22 19 32 10 26 2 42 16 1
+// 33 1 1 20 26 7 17 39 23 34 10 11 38 20
+// 11 59
+// 15 16 44 58 5 10 16 9 4 20 24
+// 37 45 41 46 8 23 59 57 51 44 59
+// 8 32
+// 28 14 24 25 2 20 1 26
+// 4 3 1 11 25 22 2 4
+// NO
+// YES
+// NO
+// NO
+// YES
+// NO
+// NO
+// YES
+// NO
+// YES
+let k = 94
+let A = [84, 2, 50, 51, 19, 58, 12, 90, 81, 68, 54, 73, 81, 31, 79, 85, 39, 2]
+let B = [53, 102, 40, 17, 33, 92, 18, 79 ,66, 23, 84, 25, 38, 43, 27, 55, 8, 19]
+
+twoArrays(k, A, B)
